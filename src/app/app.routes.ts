bfb7fd2a4b38918af6df7a5642f2dc3252547bc5
Xavi Tutusaus <xavierator@gmail.com>
2020-10-23 10:03:14 +0200

    Abrir página de naves por defecto en app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'star-ships', component: StarShipsComponent },
-    { path: '**', pathMatch:'full', redirectTo: 'routePath' }
+    { path: '**', pathMatch:'full', redirectTo: 'star-ships' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
