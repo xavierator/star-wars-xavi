41998bd2cc39b5ecb5c6f22588dc23253bfcf71c
Xavi Tutusaus <xavierator@gmail.com>
2020-10-23 09:20:42 +0200

    Cambio de logotipo, mejoras en el formato del importe en las fichas y mejora contenido página About.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+// Rutes
+import { APP_ROUTING } from './app.routes';
+
+// Serveis
+import { StarShipsService } from "./servicios/star-ships.service";
+
+// Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { StarShipsComponent } from './components/star-ships/star-ships.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    StarShipsComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [
+    StarShipsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
